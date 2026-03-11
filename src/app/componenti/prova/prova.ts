f488bbb578f3448437e369a8b0b44821a3b08b79
EssieDyva <102621809+EssieDyva@users.noreply.github.com>
2026-03-11 17:59:42 +0100

    second commit

---
+++ src/app/componenti/prova/prova.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prova',
+  standalone: false,
+  templateUrl: './prova.html',
+  styleUrl: './prova.css',
+})
+export class Prova {}
