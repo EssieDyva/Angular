bf5c10b649f7f5bbfbaaa2c249c34f6fa3e62dec
EssieDyva <st3f1703@gmail.com>
2026-03-12 17:43:01 +0100

    final commit for today

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,8 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('corso-angular');
+
+  leggoMessagioRicevuta(value:String) {
+    console.log("messaggio ricevuto: " + value)
+  }
 }
--- src/app/componenti/prova/prova.ts
+++ src/app/componenti/prova/prova.ts
@@ -1,4 +1,17 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-prova',
@@ -6,29 +19,76 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
   templateUrl: './prova.html',
   styleUrl: './prova.css',
 })
-export class Prova {
+export class Prova implements OnInit, OnChanges{
+
+  @Input() data:any;
+  @Output() mandaDatiEvent = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    console.log(this.data);
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+  }
 
   cani = [
     {
-      nome:'roger',
-      razza:'golden',
-      descrizione:'bla bla bla e compagnia...'
-    }
-  ]
+      nome: 'roger',
+      razza: 'golden',
+      descrizione: 'bla bla bla e compagnia...',
+    },
+  ];
 
-  daVedere:Boolean = true
-  
+  persone = [
+    {
+      nome: 'Luca',
+      cognome: 'Purino',
+      isOnline: true,
+      colore: 'green',
+    },
+    {
+      nome: 'Paola',
+      cognome: 'Strumel',
+      isOnline: false,
+      colore: 'red',
+    },
+    {
+      nome: 'Anna',
+      cognome: 'Berola',
+      isOnline: false,
+      colore: 'black',
+    },
+    {
+      nome: 'Marcello',
+      cognome: 'Zucchetti',
+      isOnline: true,
+      colore: 'blue',
+    },
+  ];
 
-  isDisable:Boolean = false
+  numero = 3;
 
-  hoCliccato(e:any) {
-    console.log(e)
+  coloreOn='green'
+  coloreOff='red'
+
+  daVedere: Boolean = true;
+  msgToSend="msg da Prova"
+  isDisable: Boolean = false;
+
+  hoCliccato(e: any) {
+    console.log(e);
   }
 
-  onInput(e:Event) {
-    console.log((<HTMLInputElement>e.target).value)
+  onInput(e: Event) {
+    console.log((<HTMLInputElement>e.target).value);
     // console.log(e)
   }
 
-  title:String="Titolo angular"
+  mandaDati() {
+    this.mandaDatiEvent.emit(this.msgToSend);
+  }
+
+  title: String = 'Titolo angular';
 }
